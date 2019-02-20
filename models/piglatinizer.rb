faabc4207466289046e0941d6e9d2d5bf32a7007
Thomas Pope <tpope0928@gmail.com>
2019-02-20 20:12:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,25 +1,48 @@
 class PigLatinizer
 
-  def to_pig(phrase)
-    words = phrase.split(" ")
+  attr_accessor :phrase
 
+  @@vowels = ["a","e","i","o", "u"]
+
+  def initialize
+    @phrase = phrase
   end
 
-  def piglatinize(sentence)
-    # binding.pry
-    array = to_pig(sentence)
-    array.collect {|w| new_pig(w)}.join(" ")
-
+  def piglatinize_sentence(sentence)
+    arr = sentence.split(" ")
+    new_arr = arr.map do |word|
+      self.piglatinize_word(word)
+    end
+    new_arr.join(" ")
   end
 
-  def new_pig(word)
-    letters = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-    first = letters[1]
-    second = letters[2] + (letters[3] || "")
-    if first.length>0
-      "#{second}#{first}ay"
+  def piglatinize_word(word)
+    arr = word.split('')
+
+    if @@vowels.detect{|v| v == arr[0].downcase}
+	     arr << ["w", "a", "y"]
+       new_word = arr.flatten.join("")
+     elsif @@vowels.detect{|v| v != arr[0].downcase && v == arr[1].downcase}
+       first_letter = arr.shift
+       arr << ["#{first_letter}", "a", "y"]
+       new_word = arr.flatten.join("")
+     else
+       vowel = arr.find{|letter| @@vowels.include?(letter)}
+       vowel_index = arr.index("#{vowel}")
+       prefix = arr.shift(vowel_index.to_i)
+       suffix = prefix << ["a", "y"]
+       arr << suffix
+       new_word = arr.flatten.join("")
+     end
+   new_word
+  end
+
+  def piglatinize(phrase)
+    if phrase.match(/\s/)
+      self.piglatinize_sentence(phrase)
     else
-      "#{second}way"
+      self.piglatinize_word(phrase)
     end
   end
+
 end
